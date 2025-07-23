385e9cce127e03e4b1385b37dee9386c043ed510
vaibhav <khotvaibhav900@gamil.com>
2025-07-23 11:35:20 +0530

    intial setup

---
+++ src/main/java/com/bootjava/SpBoot_CodetoLEarn/SpBootCodetoLEarnApplication.java
@@ -0,0 +1,13 @@
+package com.bootjava.SpBoot_CodetoLEarn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpBootCodetoLEarnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpBootCodetoLEarnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootjava/SpBoot_CodetoLEarn/SpBootCodetoLEarnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootjava.SpBoot_CodetoLEarn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpBootCodetoLEarnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
